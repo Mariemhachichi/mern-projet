cfa4007398fccdd48169c78a65c3e9593927060e
Mariemhachichi <hchichimariem@hotmail.fr>
2021-09-01 17:29:32 +0100

    SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
 //import './App.css';
+import React, { useEffect,useState } from "react";
 import './styles/index.scss';
-import React from 'react';
 import Routes from "./components/Routes";
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
+
+
 
 const App = () => {
+  const [uid, setUid] = useState(null);
+  
+
+   useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => {
+          setUid(res.data);
+        })
+        .catch((err) => console.log("No token"));
+    };
+    fetchToken();
+  }, [uid]);
+
   return (
-    <div>
-     <Routes />
-    </div>
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
   );
 };
+
+
 
 export default App;
