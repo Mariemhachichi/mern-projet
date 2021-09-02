ec563dd672f92f75a62f006fbe1527d5a1091d19
Mariemhachichi <hchichimariem@hotmail.fr>
2021-09-02 11:33:22 +0100

    navbar-logout

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom
 import Home from '../../pages/Home';
 import Profil from '../../pages/Profil';
 import Actualite from '../../pages/Actualite';
+import Navbar from '../Navbar';
 
 
 const index = () => {
     return (
        <Router>
+           <Navbar />
            <Switch>
                <Route path="/" exact component={Home} />
                <Route path="/profil" exact component={Profil} />
