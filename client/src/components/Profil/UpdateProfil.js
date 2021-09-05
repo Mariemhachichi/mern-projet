01e3d1a49c116698176caaee593ff77688afc899
Mariemhachichi <hchichimariem@hotmail.fr>
2021-09-05 22:22:23 +0100

    leftnav

---
+++ client/src/components/Profil/UpdateProfil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LeftNav from '../LeftNav';
+
+class UpdateProfil extends Component {
+    render() {
+        return (
+            <div className="profil-container">
+                <LeftNav />
+            </div>
+        );
+    }
+}
+
+export default UpdateProfil;
